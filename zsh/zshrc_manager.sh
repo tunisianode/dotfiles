a2e789a2dcbc95be40a147ffeb32a2dc41c7d033
tunisianode <7306517+tunisianode@users.noreply.github.com>
2019-12-05 15:31:50 +0100

    Update zshrc_manager.sh

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,11 +1,11 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 # Run tmux if exists
-if command -v tmux>/dev/null; then
-	[ -z $TMUX ] && exec tmux
-else 
-	echo "tmux not installed. Run ./deploy to configure dependencies"
-fi
+#if command -v tmux>/dev/null; then
+#	[ -z $TMUX ] && exec tmux
+#else 
+#	echo "tmux not installed. Run ./deploy to configure dependencies"
+#fi
 
 echo "Checking for updates."
 ({cd ~/dotfiles && git fetch -q} &> /dev/null)
